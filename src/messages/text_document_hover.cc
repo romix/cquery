742f0be56739617265dc8fdb74497b6a3e6ee702
romix <romixlev@web.de>
2017-12-24 18:42:04 -0800

    Improve formatting of comments shown in hover pop-ups
    - Remove different comment markers from the beginning and end of each comment line, perform some other kinds of preprocessing.
           - This is a very simplistic approach to parse comments. In the future, a more advanced real comment parser may need to be used.
    
    - Return two marked strings as a response to hover requests:
    	- The first line is the formatted comment, which is reported as a markup string of type "text". This ensures that no C/C++ highlighting is used for the comment text.
            - The second line is the declaration/signature, which is reported as a markup string of type "c" or "c++".

--- src/messages/text_document_hover.cc
+++ src/messages/text_document_hover.cc
@@ -3,36 +3,139 @@
 
 namespace {
 
-static std::string getHoverString(const optional<std::string>& hover,
-                                  const optional<std::string>& comments,
-                                  const std::string& detailed_name) {
-  // TODO: Properly format multi-line comments.
-  std::string ret;
-  if (comments) {
-    ret += *comments;
-    ret += '\n';
+/// Trim the spaces and special comment markup at the beginning of the string.
+static std::string ltrim(std::string s) {
+  auto it = s.begin();
+  for (; it != s.end() && (isspace(*it) || *it == '*' || *it == '/'); ++it) {
   }
-  return ret + hover.value_or(detailed_name);
+
+  if (it == s.end())
+    return "";
+  return std::string(it, s.end());
 }
 
-std::string GetHoverForSymbol(QueryDatabase* db, const SymbolIdx& symbol) {
+/// Trim the spaces and special comment markup at the end of the string. Leave
+/// only one space at the end.
+static std::string rtrim(std::string s) {
+  char last = '\0';
+  if (s.empty())
+    return s;
+  auto it = std::prev(s.end());
+  while ((isspace(*it) || *it == '*' || *it == '/') && it != s.begin()) {
+    last = *it;
+    --it;
+  }
+
+  if (!last)
+    return s;
+  // Leave one space at the end.
+  if (isspace(last))
+    ++it;
+  return std::string(s.begin(), it);
+}
+
+/// Split the string using a provided \p delimiter.
+std::vector<std::string> split(const std::string& s, char delimiter) {
+  std::vector<std::string> tokens;
+  std::string token;
+  std::istringstream tokenStream(s);
+  while (std::getline(tokenStream, token, delimiter)) {
+    tokens.push_back(token);
+  }
+  return tokens;
+}
+
+/// Join strings using a provided \p delimiter.
+static std::string join(std::vector<std::string> strings,
+                        const char* delimiter) {
+  std::ostringstream os;
+  bool first = true;
+  std::string last;
+  for (auto& s : strings) {
+    // Skip leading empty lines.
+    if (s.empty())
+      continue;
+    // Do not use a delimiter after newlines.
+    if (!first && last != "\n\n") {
+      os << delimiter;
+    }
+    os << s;
+    last = s;
+    first = false;
+  }
+  return os.str();
+}
+
+/// Format comments for presenting them in a nice way.
+/// FIXME: This is a very simplistic approach to parse comments.
+/// In the future, a more advanced real comment parser may need
+/// to be used.
+static std::string format_comments(const std::string& comments) {
+  // Split the comments into lines and trim them on the left.
+  auto lines = split(comments, '\n');
+
+  // Trim each line on both sides.
+  for (auto& line : lines) {
+    line = ltrim(line);
+    line = rtrim(line);
+    if (line.empty())
+      line = "\n\n";
+  }
+
+  // Remove leading new lines.
+  for (auto s = lines.begin(), e = lines.end(); s != e; ++s) {
+    if (*s != "\n\n")
+      break;
+    if (*s == "\n\n")
+      *s = "";
+  }
+
+  // Remove final new lines.
+  for (auto s = lines.rbegin(), e = lines.rend(); s != e; ++s) {
+    if (*s != "\n\n")
+      break;
+    if (*s == "\n\n")
+      *s = "";
+  }
+
+  // Now merge all lines.
+  auto formatted = join(lines, " ");
+  return formatted;
+}
+
+/// Create documentation string and hover.
+/// \returns results are stored in the output parameter \p result.
+static void getDocAndHover(const optional<std::string>& hover,
+                           const optional<std::string>& comments,
+                           const std::string& detailed_name,
+                           std::pair<std::string, std::string>& result) {
+  result.first = comments ? format_comments(*comments) : "";
+  result.second = hover ? *hover : detailed_name;
+}
+
+std::pair<std::string, std::string> GetHoverForSymbol(QueryDatabase* db,
+                                                      const SymbolIdx& symbol) {
+  std::pair<std::string, std::string> docAndHover;
   switch (symbol.kind) {
     case SymbolKind::Type: {
       QueryType& type = db->types[symbol.idx];
       if (type.def)
-        return getHoverString(type.def->hover, type.def->comments, type.def->detailed_name);
+        getDocAndHover(type.def->hover, type.def->comments,
+                       type.def->detailed_name, docAndHover);
       break;
     }
     case SymbolKind::Func: {
       QueryFunc& func = db->funcs[symbol.idx];
       if (func.def)
-        return getHoverString(func.def->hover, func.def->comments, func.def->detailed_name);
+        getDocAndHover(func.def->hover, func.def->comments,
+                       func.def->detailed_name, docAndHover);
       break;
     }
     case SymbolKind::Var: {
       QueryVar& var = db->vars[symbol.idx];
       if (var.def)
-        return getHoverString(var.def->hover, var.def->comments, var.def->detailed_name);
+        getDocAndHover(var.def->hover, var.def->comments,
+                       var.def->detailed_name, docAndHover);
       break;
     }
     case SymbolKind::File:
@@ -41,7 +144,7 @@ std::string GetHoverForSymbol(QueryDatabase* db, const SymbolIdx& symbol) {
       break;
     }
   }
-  return "";
+  return docAndHover;
 }
 
 struct Ipc_TextDocumentHover : public IpcMessage<Ipc_TextDocumentHover> {
@@ -55,7 +158,7 @@ REGISTER_IPC_MESSAGE(Ipc_TextDocumentHover);
 
 struct Out_TextDocumentHover : public lsOutMessage<Out_TextDocumentHover> {
   struct Result {
-    lsMarkedString contents;
+    std::vector<lsMarkedString> contents;
     optional<lsRange> range;
   };
 
@@ -100,11 +203,26 @@ struct TextDocumentHoverHandler : BaseMessageHandler<Ipc_TextDocumentHover> {
       if (!ls_range)
         continue;
 
-      std::string hover = GetHoverForSymbol(db, ref.idx);
-      if (!hover.empty()) {
+      // Produce two output strings. One for the documentation and one for the
+      // hover containing the singature/description.
+      auto docAndHover = GetHoverForSymbol(db, ref.idx);
+      if (!docAndHover.first.empty() || !docAndHover.second.empty()) {
+        size_t idx = 0;
         out.result = Out_TextDocumentHover::Result();
-        out.result->contents.value = hover;
-        out.result->contents.language = file->def->language;
+        if (!docAndHover.first.empty()) {
+          out.result->contents.push_back(lsMarkedString());
+          out.result->contents[idx].value = docAndHover.first;
+          out.result->contents[idx].language = "text";
+          ++idx;
+        }
+
+        if (!docAndHover.second.empty()) {
+          out.result->contents.push_back(lsMarkedString());
+          out.result->contents[idx].value = docAndHover.second;
+          out.result->contents[idx].language = file->def->language;
+          ++idx;
+        }
+
         out.result->range = *ls_range;
         break;
       }
